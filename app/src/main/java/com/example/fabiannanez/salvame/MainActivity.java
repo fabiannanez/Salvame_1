7fef7f239f62a78b462ed18869f66f052649103f
fabiannanez <fabiannanez@unicauca.edu.co>
2016-04-29 22:13:19 -0500

    viernes noche

--- app/src/main/java/com/example/fabiannanez/salvame/MainActivity.java
+++ app/src/main/java/com/example/fabiannanez/salvame/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.fabiannanez.salvame;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton text;
     ImageButton grabar;
+    ImageButton enviarAutomaticamente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final String path = Environment.getExternalStorageDirectory()+"/rec.3gp";
+
         text = (ImageButton)findViewById(R.id.texto);
         grabar = (ImageButton)findViewById(R.id.audio);
+        enviarAutomaticamente = (ImageButton)findViewById(R.id.ayuda);
 
 
         text.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent audio = new Intent(getApplicationContext(),audio.class);
                 startActivities(new Intent[]{audio});
+
+            }
+        });
+
+        enviarAutomaticamente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"MENSAJE ENVIADO"+enviarAutomaticamente.getId(),Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/fabiannanez/salvame/audio.java
+++ app/src/main/java/com/example/fabiannanez/salvame/audio.java
@@ -1,13 +1,41 @@
 package com.example.fabiannanez.salvame;
 
+import android.content.Intent;
+import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class audio extends AppCompatActivity {
+
+   ImageButton grabacion;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
+
+        grabacion= (ImageButton) findViewById(R.id.grabar);
+
+      grabacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaRecorder recorder = new MediaRecorder();
+                recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                //recorder.setOutputFile(PATH_NAME);
+                //recorder.prepare();
+                recorder.start();
+
+            }
+        });
     }
+
+
+
 }
